8845c1e1ed9be60042e52c8e69a4665bf05957bb
davi <davissbf@gmail.com>
2021-06-08 16:03:14 -0300

    consume api

--- src/components/ModalLink/index.js
+++ src/components/ModalLink/index.js
@@ -14,16 +14,16 @@ import {
 import { Feather } from '@expo/vector-icons';
 import Clipboard from 'expo-clipboard';
 
-const ModalLink = ({ onClose }) => {
+const ModalLink = ({ onClose, data }) => {
   const copyLink = () => {
-    Clipboard.setString('https://seulink.com.br');
+    Clipboard.setString(data.link);
     alert('Seu link copiado com sucesso.');
   }
 
   async function handleShare() {
     try {
       const result = await Share.share({
-        message: `Link: https://seulink.com.br`
+        message: `Link: ${data.link}`
       });
       
       if(result.action === Share.sharedAction) {
@@ -64,13 +64,13 @@ const ModalLink = ({ onClose }) => {
         </Header>
         <LinkArea>
           <Title>Link encurtado</Title>
-          <LongUrl numberOfLines={1} >https://expemplo.com.br</LongUrl>
+          <LongUrl numberOfLines={1} >{data.long_url}</LongUrl>
           <ShortLinkArea
             activeOpacity={1}
             onPress={copyLink}
           >
             <ShortLinkUrl numberOfLines={1}>
-              https://bit.ly/as32sd
+              {data.link}
             </ShortLinkUrl>
             <TouchableOpacity onPress={copyLink}>
               <Feather
